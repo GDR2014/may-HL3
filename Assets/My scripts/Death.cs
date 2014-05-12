022ba2f9112daeee098ba1e3ac135a9f13ffafdd
KHReenberg <khreenberg@gmail.com>
2014-05-12 18:27:45 +0200

    Replaced old 3D files with new 2D files

--- Assets/My scripts/Death.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-
-public abstract class Death : MonoBehaviour {
-
-    public abstract void Die();
-    
-}
--- Assets/My scripts/EnemyDeath.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public class EnemyDeath : Death {
-
-    public override void Die() {
-        Destroy(gameObject);
-    }
-
-}
