01378e34d4392c29f23ed98301e601d9191276fb
KHReenberg <khreenberg@gmail.com>
2014-05-13 18:55:09 +0200

    Makes the player move

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public bool isWalking = false;
+    public Animator animator;
+    public float MOVE_SPEED = 1.0f;
+    private const float MOVE_CONST = 0.1f;
+
+    void Update() {
+        Vector2 velocity = new Vector2( Input.GetAxis( "Horizontal" ), Input.GetAxis( "Vertical" ) );
+        velocity.Normalize();
+        Vector2 position = transform.position;
+        position += velocity * ( MOVE_CONST * MOVE_SPEED );
+        transform.position = position;
+    }
+}
