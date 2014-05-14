dd3cbff356a6ca1e47f8f1bf62bad67f8c328c90
KHReenberg <khreenberg@gmail.com>
2014-05-14 11:39:26 +0200

    Adds sprite and attack animation

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+
+    public Animator animator;
+    public float attackDelay;
+    public bool canAttack = true;
+
+    void Update() {
+        if( Input.GetMouseButtonDown( 0 ) && canAttack ) attack();
+
+    }
+
+    void attack() {
+        animator.SetTrigger( "doAttack" );
+    }
+
+    IEnumerator beginCooldown() {
+        canAttack = false;
+        yield return new WaitForSeconds( attackDelay );
+        canAttack = true;
+    }
+}
