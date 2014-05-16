32068061fdbcfda448025f7317ac66ec33263fa8
KHReenberg <khreenberg@gmail.com>
2014-05-16 12:56:26 +0200

    Begins work on FoV. Updating Unity...

---
+++ Assets/Scripts/FieldOfVision.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class FieldOfVision : MonoBehaviour {
+
+    public IWatcher Watcher { get; set; }
+
+    void Start() {
+        GetComponent<Collider2D>().isTrigger = true;
+    }
+
+    void OnTriggerEnter2D( Collider2D other ) {
+        Watcher.onNotice(other);
+    }
+
+    void OnTriggerStay2D( Collider2D other ) {
+        Watcher.onSee( other );
+    }
+
+    void OnTriggerExit2D( Collider2D other ) {
+        Watcher.onLost( other ); // Not working properly because of Unity bugs
+    }
+}
---
+++ Assets/Scripts/Headcrab.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Headcrab : MonoBehaviour, IWatcher {
+
+    private List<FieldOfVision> _fovs; 
+
+    void Awake() {
+        _fovs = new List<FieldOfVision>(GetComponentsInChildren<FieldOfVision>());
+        foreach( FieldOfVision fov in _fovs ) {
+            Debug.Log("Setting watcher of " + fov.transform.name + " to " + transform.name);
+            fov.Watcher = this;
+        }
+    }
+
+    public void onNotice( Collider2D other ) {
+    }
+
+    public void onSee( Collider2D other ) {
+    }
+
+    public void onLost( Collider2D other ) {
+        Debug.Log( transform.name + " lost sight of " + other.transform.name );
+    }
+}
