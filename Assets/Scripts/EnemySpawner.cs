46d131042f44e19165625fa94553cb9732248416
KHReenberg <khreenberg@gmail.com>
2014-06-02 18:11:33 +0200

    Back after long break.

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Scripts.Non_behaviours;
+using System.Collections.Generic;
+
+public class EnemySpawner : MonoBehaviour {
+
+    public Vector2[] spawnPoints;
+    public Bounds spawnAreaSize;
+
+    public AbstractEnemy[] enemies;
+    public Transform target;
+
+    public float spawnInterval = 2f;
+    public float minDelay = .4f;
+
+    private Camera cam;
+    private Plane[] planes;
+
+    private List<Vector2> spawnPointBuffer;
+
+	private void Start () {
+        Debug.Log("Start called!");
+        spawnPointBuffer = new List<Vector2>(spawnPoints.Length);
+        cam = Camera.main;
+        planes = GeometryUtility.CalculateFrustumPlanes(cam);
+        StartCoroutine(EnemySpawnRoutine());
+    }
+
+    private IEnumerator EnemySpawnRoutine()
+    {
+        Debug.Log("Coroutine started!");
+        spawnPointBuffer.Clear();
+        yield return new WaitForSeconds(spawnInterval);
+        foreach(var point in spawnPoints ) if( !IsInView(point) ) spawnPointBuffer.Add(point);
+        if (spawnPointBuffer.Count == 0) StartCoroutine(EnemySpawnRoutine());
+        int idx = Random.Range(0, spawnPointBuffer.Count);
+        SpawnAt(spawnPointBuffer[idx], enemies[0]);
+        spawnInterval *= .9999f;
+        spawnInterval = Mathf.Max(spawnInterval, minDelay);
+        StartCoroutine(EnemySpawnRoutine());
+    }
+
+    private bool IsInView(Vector2 spawner)
+    {
+        spawnAreaSize.center = spawner;
+        return GeometryUtility.TestPlanesAABB(planes, spawnAreaSize);
+    }
+
+    private void SpawnAt(Vector2 position, AbstractEnemy enemy)
+    {
+        Headcrab crab = (Headcrab)Instantiate(enemy, position, Quaternion.identity);
+        crab.target = target;
+    }
+}
--- Assets/Scripts/Headcrab.cs
+++ Assets/Scripts/Headcrab.cs
@@ -1,16 +1,30 @@
-﻿using System.Collections.Generic;
+﻿using Assets.Scripts.Non_behaviours;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class Headcrab : MonoBehaviour, IWatcher {
+public class Headcrab : AbstractEnemy, IWatcher {
 
-    private List<FieldOfVision> _fovs; 
+    private List<FieldOfVision> _fovs;
+    public Animator anim;
+
+    public float decayDelay = 0f;
+    public float decayTime = 2f;
+
+    public float Speed = 10f;
+
+    public Transform target;
 
     void Awake() {
         _fovs = new List<FieldOfVision>(GetComponentsInChildren<FieldOfVision>());
         foreach( FieldOfVision fov in _fovs ) {
-            Debug.Log("Setting watcher of " + fov.transform.name + " to " + transform.name);
             fov.Watcher = this;
         }
+    }
+
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target.position, Speed * Time.deltaTime);
     }
 
     public void onNotice( Collider2D other ) {
@@ -20,6 +34,30 @@ public class Headcrab : MonoBehaviour, IWatcher {
     }
 
     public void onLost( Collider2D other ) {
-        Debug.Log( transform.name + " lost sight of " + other.transform.name );
+    }
+
+    protected override void Hurt(int dmgAmount)
+    {
+        anim.SetTrigger("doHurt");
+    }
+
+    protected override void Die()
+    {
+        collider2D.isTrigger = true;
+        StartCoroutine(DecayAndDestroy());
+    }
+
+    private IEnumerator DecayAndDestroy()
+    {
+        Material m = transform.FindChild("Sprite").renderer.material;
+        yield return new WaitForSeconds(decayDelay);
+        float endTime = Time.time + decayTime;
+        while (m.color.a > 0)
+        {
+            float t = 1 - (endTime - Time.time) / decayTime;
+            m.color = Color.Lerp(m.color, Color.clear, t);
+            yield return null;
+        }
+        Destroy(this.gameObject);
     }
 }
